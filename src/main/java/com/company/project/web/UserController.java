e2940929e151137506ee0c24cc6eb2bb0aefd5a7
oneday2321 <oneday2321@163.com>
2024-11-18 17:46:03 +0800

    test

--- src/main/java/com/company/project/web/UserController.java
+++ src/main/java/com/company/project/web/UserController.java
@@ -46,7 +46,7 @@ public class UserController {
         return ResultGenerator.genSuccessResult(user);
     }
 
-//    获取列表
+//    获取列表 001
     @PostMapping("/list")
     public Result list(@RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "0") Integer size) {
         PageHelper.startPage(page, size);
