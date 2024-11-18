da9d0396bcf8489dfa09716db2ada86e1f5d82b2
oneday2321 <oneday2321@163.com>
2024-11-18 17:33:36 +0800

    全部提交

---
+++ src/main/java/com/company/project/service/UserService.java
@@ -0,0 +1,11 @@
+package com.company.project.service;
+import com.company.project.model.User;
+import com.company.project.core.Service;
+
+
+/**
+ * Created by CodeGenerator on 2024/11/18.
+ */
+public interface UserService extends Service<User> {
+
+}
