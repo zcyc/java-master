8bcf22e1c542c268c21dbd971c2c1887e37f3e79
for shell <fromShell@paas.com>
2024-12-05 17:35:17 +0800

    Update database configuration for development environment to use environment variables

--- src/test/resources/demo-user.sql
+++ src/test/resources/demo-user.sql
@@ -18,6 +18,7 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
+
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
