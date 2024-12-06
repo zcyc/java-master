66473ad8f995a01a6c541ab72079554112a5ede5
for shell <fromShell@paas.com>
2024-12-06 15:52:43 +0800

    Implement user login endpoint with credential validation in UserService.

--- src/main/java/com/company/project/service/UserService.java
+++ src/main/java/com/company/project/service/UserService.java
@@ -2,10 +2,24 @@ package com.company.project.service;
 import com.company.project.model.User;
 import com.company.project.core.Service;
 
-
 /**
  * Created by CodeGenerator on 2024/11/18.
  */
 public interface UserService extends Service<User> {
+    
+    /**
+     * Adds a user to the system.
+     *
+     * @param user The user to be added.
+     */
     void addUser(User user);
+
+    /**
+     * Validates the credentials of a user.
+     *
+     * @param username The username of the user.
+     * @param password The password of the user.
+     * @return true if the credentials are valid, false otherwise.
+     */
+    boolean validateCredentials(String username, String password);
 }
